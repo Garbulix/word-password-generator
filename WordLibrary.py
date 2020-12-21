2e9a7890c1164b9ca75c231340f2d4a1586cc950
Garbulix <garbiec@bartek.me>
2020-12-21 17:16:54 +0100

    WordLibrary can import now

--- WordLibrary.py
+++ WordLibrary.py
@@ -3,5 +3,15 @@ import random
 class WordLibrary():
     """class with words loaded from given file"""
 
-    def __init__(self):
-        pass
+    def __init__(self, path = ''):
+        self.is_imported = False
+        self.dictionary_name = ''
+        self.words = []
+
+        if path != '':
+            with open(path) as dict_file:
+                self.words = dict_file.read().splitlines()
+                self.is_imported = True
+                self.dictionary_name = self.words[0]
+                self.words = self.words[2:]     # get rid of dict name and '#' line
+
