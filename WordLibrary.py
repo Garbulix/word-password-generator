191c3141a34decd213db0a0b6e23abe5686da977
Garbulix <garbiec@bartek.me>
2020-12-22 09:30:20 +0100

    WordLibrary improved (overlaoded operators)

--- WordLibrary.py
+++ WordLibrary.py
@@ -9,9 +9,20 @@ class WordLibrary():
         self.words = []
 
         if path != '':
-            with open(path) as dict_file:
-                self.words = dict_file.read().splitlines()
-                self.is_imported = True
-                self.dictionary_name = self.words[0]
-                self.words = self.words[2:]     # get rid of dict name and '#' line
+            try:
+                with open(path) as dict_file:
+                    self.words = dict_file.read().splitlines()
+                    self.is_imported = True
+                    self.dictionary_name = self.words[0]
+                    self.words = self.words[2:]     # toss dict name and '#' line
+            except IOError:
+                print('File not found, sorry!')
 
+    def __getitem__(self, index):
+        return self.words[index]
+
+    def __len__(self):
+        return len(self.words)
+
+    def clear(self):
+        self.words.clear()
