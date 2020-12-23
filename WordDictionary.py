c6b79387821186b856c34e37a57537a5fd4d9032
Garbulix <garbiec@bartek.me>
2020-12-23 19:04:20 +0100

    simplify Dictionary, add comments to dict files

--- WordDictionary.py
+++ WordDictionary.py
@@ -4,26 +4,32 @@ class WordDictionary():
     """class with words loaded from specified and prepared file"""
 
     def __init__(self, path = ''):
-        self.is_imported = False
-        self.dictionary_name = ''
-        self.words = []
+        """class is aimed to be read-only"""
+
+        self._words = []
 
         if path != '':
             try:
                 with open(path) as dict_file:
-                    self.words = dict_file.read().splitlines()
-                    self.is_imported = True
-                    self.dictionary_name = self.words[0]
-                    self.words = self.words[2:]     # toss dict name and '#' line
+                    all_file_lines = dict_file.read().splitlines()
+                    for line in all_file_lines:
+                        # toss all lines starting with '#'
+                        if line[0] == '#':
+                            continue
+                        else:
+                            self._words.append(line)
             except FileNotFoundError:
                 print('ERROR: Dictionary file not found, sorry!')
                 raise SystemExit()
 
     def __getitem__(self, index):
-        return self.words[index]
+        """overload [] operator - only for getting word under given index"""
+        return self._words[index]
 
     def __len__(self):
-        return len(self.words)
+        """return number of _words in dictionary"""
+        return len(self._words)
 
     def clear(self):
-        self.words.clear()
+        """clears/resets the container"""
+        self._words.clear()
