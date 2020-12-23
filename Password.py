26fba3f2475c1fa9dbabbfe9513778ed3030dfe7
Garbulix <garbiec@bartek.me>
2020-12-23 10:26:00 +0100

    making Password class functional

--- Password.py
+++ Password.py
@@ -1,20 +1,41 @@
 import random
-import WordDictionary
-
+import WordDictionary as wd
 
 class WordPassword():
-    def __init__(self, dictionary=WordDictionary(), no_of_words=0, capitalize=False, separator='-', numbers_to_insert=0):
+    def __init__(self, dictionary=wd.WordDictionary(), no_of_words=0, capitalize=False, 
+                separator='-', numbers_to_insert=0):
         self.dictionary = dictionary
         self.no_of_words = no_of_words
         self.capitalize = capitalize
         self.separator = separator
         self.numbers_to_insert = numbers_to_insert
+        
+        self.password = ''
+        self.is_generated = False
 
         if len(dictionary) > 0:
-            # create first password 
-            pass
+            self.regenerate_password()
+            self.is_generated = True
 
     def regenerate_password(self):
+        """generate new password with whole new drawn words"""
+
         random.seed()
+        available_words = len(self.dictionary)
+
+        # randomly chose words to use without copying all dictionary
+        drawn_words_indexes = random.sample(list(range(available_words)), self.no_of_words)
+        words_to_use = [self.dictionary[word_index] for word_index in drawn_words_indexes]
+
+        if self.capitalize:
+            for word_index in range(len(words_to_use)):
+                words_to_use[word_index] = words_to_use[word_index].capitalize()
+
+        if self.numbers_to_insert:
+            words_to_use_indexes = list(range(len(words_to_use)))
+            words_to_put_numbers_to = random.choices(words_to_use_indexes, k=self.numbers_to_insert)
+            for word_index in words_to_put_numbers_to:
+                words_to_use[word_index] += str(random.randint(0, 9))
         
+        self.password = self.separator.join(words_to_use)
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-import random
+import Password
 import WordDictionary
 
 """
@@ -20,28 +20,7 @@ NUMBERS_TO_INSERT = int(cli_arguments[4])
 PATH = '/home/bartek/Programowanie/word-password-generator/dictionaries/RANDOM.txt'
 # CONFIGS #
 
-# TODO clean that mess
-def generate_password(word_dictionary, no_of_words):
-    drawed_words = []
-    dict_length = len(word_dictionary)
-
-    for word in range(no_of_words):
-        # TODO reject duplicates
-        randomly_chosen_word_index = random.randint(0, dict_length - 1)
-        drawed_words.append(word_dictionary[randomly_chosen_word_index])
-
-    if NUMBERS_TO_INSERT > 0:
-        # add a random numbers after a random words
-        for number in range(NUMBERS_TO_INSERT):
-            randomly_chosen_word_index = random.randint(0, len(drawed_words) - 1)
-            drawed_words[randomly_chosen_word_index] += str(random.randint(0, 9))
-
-    if CAPITALIZE:
-        words_to_use_capitalized = [word.capitalize() for word in drawed_words]
-        return SEPARATOR.join(words_to_use_capitalized)
-    else:
-        return SEPARATOR.join(drawed_words)
-
 # ===================================
-
-print(generate_password(WordDictionary.WordDictionary(PATH), WORDS_IN_PASS))
+dictionary = WordDictionary.WordDictionary(PATH)
+sample_password = Password.WordPassword(dictionary, WORDS_IN_PASS, CAPITALIZE, SEPARATOR, NUMBERS_TO_INSERT)
+print(sample_password.password)
