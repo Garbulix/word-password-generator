ba27525ffd4c22d7d7a04937822b4b97014732b7
Garbulix <garbiec@bartek.me>
2020-12-23 19:03:41 +0100

    user now can pass dictionary path

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ import WordDictionary
 TODO class and method comments
 """
 
+# CLI arguments #
 cli_arguments = sys.argv[:]
-
-# CONFIGS #
-WORDS_IN_PASS = int(cli_arguments[1])
-SEPARATOR = cli_arguments[2]
-CAPITALIZE = bool(cli_arguments[3])
-NUMBERS_TO_INSERT = int(cli_arguments[4])
-PATH = '/home/bartek/Programowanie/word-password-generator/dictionaries/RANDOM.txt'
-# CONFIGS #
+DICT_PATH = cli_arguments[1]
+WORDS_IN_PASS = int(cli_arguments[2])
+SEPARATOR = cli_arguments[3]
+CAPITALIZE = bool(cli_arguments[4])
+NUMBERS_TO_INSERT = int(cli_arguments[5])
+# CLI arguments #
 
 # ===================================
-dictionary = WordDictionary.WordDictionary(PATH)
+dictionary = WordDictionary.WordDictionary(DICT_PATH)
 sample_password = Password.WordPassword(dictionary, WORDS_IN_PASS, CAPITALIZE, SEPARATOR, NUMBERS_TO_INSERT)
 print(sample_password.password)
