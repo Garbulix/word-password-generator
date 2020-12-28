04efc052e1a521bee434488d103c35df5c6d0a1c
Garbulix <garbiec@bartek.me>
2020-12-28 11:57:52 +0100

    unit tests prototypes

---
+++ test_Password.py
@@ -0,0 +1,20 @@
+import unittest
+
+from Password import WordPassword
+
+class TestWordPassword(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_no_of_words(self):
+        """testing wheter number of words is correct"""
+        pass
+
+    def test_no_of_numbers(self):
+        """test if there are requested number of digits"""
+        pass
+
+    def test_capitalize(self):
+        """tests whether every word is capitalized/non-capitalized correctly"""\
+        pass
---
+++ test_WordDictionary.py
@@ -0,0 +1,16 @@
+import unittest
+
+import WordDictionary as wd
+
+class TestWordDictionary(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_loading_comments(self):
+        """test whether class correctly omits commented lines"""
+        pass
+
+    def test_no_of_lines(self):
+        """check if number of loaded words == number of uncommented lines in file"""
+        pass
