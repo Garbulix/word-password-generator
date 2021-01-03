8472123ffe691b3a3b88ebae035e605f71f1cc44
Garbulix <garbiec@bartek.me>
2021-01-03 18:48:09 +0100

    first works towards bettfirst works towards better cli parserer cli parser

---
+++ cli_parser.py
@@ -0,0 +1,33 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Generate secure and easy to learn passphrases.")
+
+parser.add_argument('--dict', 
+                    required=True, 
+                    type=str,
+                    help="Specify which wordlist you'd like to use.")
+parser.add_argument('--words', 
+                    required=True, 
+                    type=int,
+                    help="Specify how many words you need to use in your passphrase.")
+parser.add_argument('--sep', 
+                    required=False, 
+                    default='-',
+                    type=str,
+                    help="Which word separator should be used.")
+parser.add_argument('--cap', 
+                    required=False, 
+                    default=False,
+                    type=bool,
+                    help=("Define whether used words should be capitalized "
+                          "(first letter big). Put True or False"))
+parser.add_argument('--number', 
+                    required=False, 
+                    default=False,
+                    type=bool,
+                    help=("Decide if you want to put one digit somewhere in the password. "
+                          "Put True of False"))
+
+args = parser.parse_args()
+
+print(vars(args))
