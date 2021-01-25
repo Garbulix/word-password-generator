173bae019bf0f361e72304b32e014e368eaa4f7a
Garbulix <garbiec@bartek.me>
2021-01-25 06:56:57 +0100

    add separator function

--- res/GUI.py
+++ res/GUI.py
@@ -1,7 +1,5 @@
 import gi
 from gi.repository import Gtk
-#import sys 
-# TODO is sys needed?
 
 import res.InterfaceBridge
 
@@ -14,7 +12,7 @@ class MainWindow(Gtk.Window):
     guitext_no_of_words = "Words in password"
     guitext_capitalize = "Capitalize words"
     guitext_separator = "Separator"
-    guitext_numbers_to_insert = "Insert digit"
+    guitext_numbers_to_insert = "Insert number"
     guitext_generate_button = "Generate password"
     guitext_select_dict = "Select dictionary"
 
@@ -58,20 +56,20 @@ class MainWindow(Gtk.Window):
         self.passphrase_preferences_vbox.pack_start(self.selected_dict_hbox, False, False, 0)
         self.passphrase_preferences_vbox.pack_start(self.demanded_words_hbox, False, False, 0)
         self.passphrase_preferences_vbox.pack_start(self.capitalize_hbox, False, False, 0)
-        self.passphrase_preferences_vbox.pack_start(self.separator_hbox, False, False, 0)
         self.passphrase_preferences_vbox.pack_start(self.insert_number_hbox, False, False, 0)
+        self.passphrase_preferences_vbox.pack_start(self.separator_hbox, False, False, 0)
 
         ### selected dictionary box
         selected_dict_label = Gtk.Label(label=self.guitext_select_dict)
-        dict_chose_box = Gtk.ComboBoxText()
-        dict_chose_box.set_entry_text_column(0)
-        dict_chose_box.connect("changed", self._on_dict_chosed)
+        select_dict_combo = Gtk.ComboBoxText()
+        select_dict_combo.set_entry_text_column(0)
+        select_dict_combo.connect("changed", self._on_dict_chosed)
         for dictionary in controller.get_available_dictionaries():
-            dict_chose_box.append_text(dictionary)
-        dict_chose_box.set_active(0)
-        self._on_dict_chosed(dict_chose_box)  # set dictionary
-        self.separator_hbox.pack_start(selected_dict_label, False, False, 0)
-        self.separator_hbox.pack_end(dict_chose_box, False, False, 0)
+            select_dict_combo.append_text(dictionary)
+        select_dict_combo.set_active(0) # TODO load from InterfaceBridge 
+        self._on_dict_chosed(select_dict_combo)  # set dictionary
+        self.selected_dict_hbox.pack_start(selected_dict_label, False, False, 0)
+        self.selected_dict_hbox.pack_end(select_dict_combo, False, False, 0)
 
         ### demanded words box
         demanded_words_label = Gtk.Label(label=self.guitext_no_of_words)
@@ -101,6 +99,17 @@ class MainWindow(Gtk.Window):
         self.insert_number_hbox.pack_start(insert_number_label, False, False, 0)
         self.insert_number_hbox.pack_end(self.insert_number_switch, False, False, 0)
 
+        ### chosed separator box
+        separator_label = Gtk.Label(label=self.guitext_separator)
+        separator_chose_box = Gtk.ComboBoxText()
+        separator_chose_box.set_entry_text_column(0)
+        separator_chose_box.connect("changed", self._on_separator_chosed)
+        for separator in controller.get_recommended_separators():
+            separator_chose_box.append_text(separator)
+        separator_chose_box.set_active(0)   # TODO load from InterfaceBridge 
+        self.separator_hbox.pack_start(separator_label, False, False, 0)
+        self.separator_hbox.pack_end(separator_chose_box, False, False, 0)
+
         # PACKING ALL TOGETHER
         main_vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
         self.add(main_vbox)
@@ -126,3 +135,6 @@ class MainWindow(Gtk.Window):
 
     def _on_number_switch_toggled(self, switch, gparam):
         self._controller.set_arguments(if_insert_number=switch.get_active())
+
+    def _on_separator_chosed(self, combo):
+        self._controller.set_arguments(separator=combo.get_active_text())
